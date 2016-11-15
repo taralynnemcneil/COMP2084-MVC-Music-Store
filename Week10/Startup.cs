374be5be9ad2743a0c3aa4ce81d10b20f975fd78
Tara McNeil <taralynnemcneil@gmail.com>
2016-11-15 10:18:58 -0500

    Add project files.

---
+++ Week10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week10.Startup))]
+namespace Week10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
